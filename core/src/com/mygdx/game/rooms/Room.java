66de2f4a2938b9c37cf12ecb0414ef1ee33251d4
Mauk <maukmul@gmail.com>
2022-06-03 19:18:47 +0200

    :construction: WIP

---
+++ core/src/com/mygdx/game/rooms/Room.java
@@ -0,0 +1,24 @@
+package com.mygdx.game.rooms;
+
+public class Room {
+
+    private final int TILE_PIXEL_WIDTH = 80;
+    private final int TILE_PIXEL_HEIGHT = 80;
+
+
+    // Assuming 1920x1080
+    // MAX_TILE_WIDTH = 24;
+    // MAX_TILE_HEIGHT = 13.5;
+    // 0, 0 is top left
+    private RoomTile[][] tiles = new RoomTile[24][13];
+
+    public Room() {
+
+    }
+
+
+
+
+
+
+}
---
+++ core/src/com/mygdx/game/rooms/RoomTileType.java
@@ -0,0 +1,10 @@
+package com.mygdx.game.rooms;
+
+public enum RoomTileType {
+
+    FLOOR,
+    OBSTACLE,
+    WALL_LEFT,
+    WALL_RIGHT
+
+}
