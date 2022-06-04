c441aa9150792953955c727c1bf1466344e5a23f
gerryflap <gerry2211@gmail.com>
2022-06-04 16:25:45 +0200

    Setup

---
+++ core/src/gamejam/event/EfficientCollisionHandler.java
@@ -0,0 +1,12 @@
+package gamejam.event;
+
+public class EfficientCollisionHandler {
+    private static EfficientCollisionHandler efficientCollisionHandler = null;
+
+    public static EfficientCollisionHandler getInstance() {
+        if (efficientCollisionHandler == null) {
+
+        }
+    }
+
+}
