b31351498e33b766af954c5b292a56da612375e0
H.C. Jansen <chris9898@live.nl>
2022-06-05 17:34:44 +0200

    enemy death animations and explosions

--- core/src/gamejam/objects/collidable/explosion/Explosion.java
+++ core/src/gamejam/objects/collidable/explosion/Explosion.java
@@ -1,5 +1,9 @@
 package gamejam.objects.collidable.explosion;
 
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Animation;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import gamejam.Camera;
 import gamejam.event.events.CollisionEvent;
 import gamejam.objects.collidable.Collidable;
 import gamejam.objects.collidable.Player;
@@ -15,14 +19,30 @@ public class Explosion extends Collidable {
 
     protected int frame = 0;
 
+    private final Texture spriteSheet;
+    private Animation<TextureRegion> animation;
+    private TextureRegion currentSprite;
+
     public Explosion(float x, float y, float spriteWidth, float spriteHeight, float collisionWidth,
-                     float collisionHeight, float damage, boolean hurtsPlayer, float durationMS) {
+                     float collisionHeight, float damage, boolean hurtsPlayer, float durationMS,  Texture spriteSheet, int keyFrameWidth, int keyFrameHeight) {
         super(spriteWidth, spriteHeight, collisionWidth, collisionHeight);
         this.damage = damage;
         this.hurtsPlayer = hurtsPlayer;
         this.durationMS = durationMS;
         this.timeLeft = durationMS;
+        this.spriteSheet = spriteSheet;
+        TextureRegion[] frames = TextureRegion.split(spriteSheet, keyFrameWidth, keyFrameHeight)[0];
+        animation = new Animation<>(0.2f, frames);
         setPosition(x, y);
+    }
+
+    public Explosion(float x, float y, float spriteWidth, float spriteHeight, float collisionWidth,
+                     float collisionHeight, float damage, boolean hurtsPlayer, float durationMS) {
+        this(x,y,spriteWidth,spriteHeight,collisionWidth,collisionHeight,damage,hurtsPlayer,durationMS,new Texture("entity/explosion.png"), 40, 32);
+    }
+
+    public Explosion(float x, float y) {
+        this(x,y, 40 * 5, 32 * 5, 40 * 5, 32 * 5, 0, false, 600, new Texture("entity/explosion.png"), 40, 32);
     }
 
     @Override
@@ -53,6 +73,15 @@ public class Explosion extends Collidable {
         if (!hurtsPlayer && other instanceof AbstractEnemy) {
             ((AbstractEnemy) other).damage(damage);
         }
+    }
+
+    @Override
+    public void draw(Camera camera) {
+        TextureRegion[] keyFrames = animation.getKeyFrames();
+        int frame = Math.min(keyFrames.length - 1, (keyFrames.length - 1) - ((int) (keyFrames.length * (timeLeft / durationMS))));
+        currentSprite = keyFrames[frame];
+
+        camera.draw(currentSprite, x - spriteWidth / 2, y, spriteWidth, spriteHeight, false, false);
     }
 
     @Override
