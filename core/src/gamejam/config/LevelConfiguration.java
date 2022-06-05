2b47ac845ae767da3493c65e1ad70feaedb803c7
Mauk <maukmul@gmail.com>
2022-06-05 18:10:14 +0200

    added random seed instead of static seed

--- core/src/gamejam/config/LevelConfiguration.java
+++ core/src/gamejam/config/LevelConfiguration.java
@@ -1,10 +1,12 @@
 package gamejam.config;
 
+import java.util.Random;
+
 public class LevelConfiguration {
 
     public static int N_ROOMS = 16;
     public static float UPGRADE_ROOM_CHANCE = 0.25f;
-    public static int SEED = 15;
+    public static int SEED = new Random().nextInt(1000000);
     public static double STRAIGHTNESS_FACTOR = 0.8;
     public static float SPAWN_RATE = 0.01f;
     public static float SPAWN_RATE_GROWTH = 1.1f;
