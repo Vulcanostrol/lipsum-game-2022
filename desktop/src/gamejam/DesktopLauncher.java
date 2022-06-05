c9d1b87344512d3fcc304df7a8cba3c3c50ff0fd
Koen van den Brink <koen.brink.vd@gmail.com>
2022-06-05 18:28:12 +0200

    Title

--- desktop/src/gamejam/DesktopLauncher.java
+++ desktop/src/gamejam/DesktopLauncher.java
@@ -8,7 +8,7 @@ public class DesktopLauncher {
 	public static void main (String[] arg) {
 		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
 		config.setForegroundFPS(60);
-		config.setTitle("Test gamejam.Game");
+		config.setTitle("Chipsum");
 		config.setWindowedMode(1280, 720);
 		new Lwjgl3Application(new Main(), config);
 	}
